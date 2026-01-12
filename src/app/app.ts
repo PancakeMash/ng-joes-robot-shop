7a8cd237718f81a8d5776db37a5d173d5ec3dc22
PancakeMash <arpandevofficial@gmail.com>
2026-01-12 20:37:49 +0000

    A: Worked on reinforcing concepts: components, making sure to import them into app.ts otherwise it doesn't get imported. Keeping track of angular.json for assets

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './home/home'; 
+import { Catalog } from './catalog/catalog';
+import { SiteHeaderComponent } from './site-header/site-header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home, Catalog, SiteHeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
